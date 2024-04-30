4b65c2b9b5077be3b217a1fe54d8e989cb65633c
Natalia <icxodnik@gmail.com>
2024-04-29 23:45:05 -0400

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import SearchBar from 'components/Searchbar';
+import Loader from 'components/Loader';
 import ImageGallery from 'components/ImageGallery';
 import Button from 'components/Button';
 import { Component } from 'react';
@@ -10,6 +11,7 @@ export class App extends Component {
     hasNextPage: false,
     query: '',
     images: [],
+    isLoading: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
@@ -25,10 +27,12 @@ export class App extends Component {
     this.setState({
       images: result,
       hasNextPage: hasNextPage,
+      isLoading: false,
     });
   }
 
   handleSearch = async () => {
+    this.setState({ isLoading: true });
     const result = await getItems(this.state.query, this.state.page);
     this.updateImages(result.data, result.hasNextPage);
   };
@@ -38,10 +42,12 @@ export class App extends Component {
       return {
         images: prevState.images.concat(result),
         hasNextPage: hasNextPage,
+        isLoading: false,
       };
     });
   }
   handleLoadMore = async () => {
+    this.setState({ isLoading: true });
     const result = await getItems(this.state.query, ++this.state.page);
     this.addImages(result.data, result.hasNextPage);
   };
@@ -55,6 +61,7 @@ export class App extends Component {
   render() {
     return (
       <div className="app">
+        <Loader hide={!this.state.isLoading} />
         <SearchBar
           // handleSearch={this.handleSearch}
           handleQueryChange={this.handleQueryChange}
--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 
 export default class Loader extends Component {
   //   static propTypes = { second: third };
 
+  getClasses() {
+    let btnClass = 'loader';
+    if (this.props.hide) btnClass += ' hidden';
+    return btnClass;
+  }
   render() {
-    return <div>Loader</div>;
+    return (
+      <div className={this.getClasses()}>
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
   }
 }
